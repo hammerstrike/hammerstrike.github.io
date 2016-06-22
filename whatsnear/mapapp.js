e6bd868436a7aede53cdae40c17531b50ab55f1c
Amardeep K <hammerstrike@users.noreply.github.com>
2016-06-22 19:55:35 +0530

    Places code added
    Places code added

--- whatsnear/mapapp.js
+++ whatsnear/mapapp.js
@@ -1,43 +1,119 @@
-// Note: This example requires that you consent to location sharing when
-// prompted by your browser. If you see the error "The Geolocation service
-// failed.", it means you probably did not give permission for the browser to
-// locate you.
+$(document).ready(function () {
 
-function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-            lat: -34.397,
-            lng: 150.644
-        }, 
-        zoom: 15
-    });
-    var infoWindow = new google.maps.InfoWindow({
-        map: map
-    });
+	var map,
+		infowindow,
+		myLocation,
+		map, 
+		marker,
+		mapBlock = $('#map')[0],
+		input = document.getElementById("pac-input");
+	
+	var url = window.location.hash;
+	var hash = url.substring(url.indexOf("#")+1);
+	
+	//Check for Browser support
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function (position) {			
+			myLocation = {
+				lat: position.coords.latitude,
+				lng: position.coords.longitude
+			};
+			embedMap(myLocation);
+		});
+	} else {
+		mapBlock.text('Your browser is out of fashion, there\'s no geolocation!');
+	};
+	
+	//Embed map
+	function embedMap(latlng) {
+		console.log(latlng);
 
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
+		map = new google.maps.Map(mapBlock, {
+			center: latlng,
+			zoom: 15
+		});
 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            map.setCenter(pos);
-        }, function () {
-            handleLocationError(true, infoWindow, map.getCenter());
-        });
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-    }
-}
+		infowindow = new google.maps.InfoWindow();
+		var marker = new google.maps.Marker({
+		    map: map,
+		    anchorPoint: new google.maps.Point(0, -29)
+		  });
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-        'Error: The Geolocation service failed.' :
-        'Error: Your browser doesn\'t support geolocation.');
-}
+		var service = new google.maps.places.PlacesService(map);
+		service.nearbySearch({
+			location: latlng,
+			radius: 500,
+			types: [hash]
+		}, callback);
+
+		//map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+		var autocomplete = new google.maps.places.Autocomplete(input);
+  		autocomplete.bindTo('bounds', map);
+
+
+  		autocomplete.addListener('place_changed', function() {
+  			infowindow.close();
+  			marker.setVisible(false);
+
+  			var place = autocomplete.getPlace();
+		    if (!place.geometry) {
+		      window.alert("Autocomplete's returned place contains no geometry");
+		      return;
+		    }
+
+		    // If the place has a geometry, then present it on a map.
+		    if (place.geometry.viewport) {
+		      map.fitBounds(place.geometry.viewport);
+		    } else {
+		      map.setCenter(place.geometry.location);
+		      map.setZoom(17);  // Why 17? Because it looks good.
+		    }
+		    marker.setIcon(/** @type {google.maps.Icon} */({
+		      url: place.icon,
+		      size: new google.maps.Size(71, 71),
+		      origin: new google.maps.Point(0, 0),
+		      anchor: new google.maps.Point(17, 34),
+		      scaledSize: new google.maps.Size(35, 35)
+		    }));
+		    marker.setPosition(place.geometry.location);
+		    marker.setVisible(true);
+
+		    var address = '';
+		    if (place.address_components) {
+		      address = [
+		        (place.address_components[0] && place.address_components[0].short_name || ''),
+		        (place.address_components[1] && place.address_components[1].short_name || ''),
+		        (place.address_components[2] && place.address_components[2].short_name || '')
+		      ].join(' ');
+		    }
+
+		    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+		    infowindow.open(map, marker);
+  		})
+
+	};
+	
+	function callback(results, status) {
+		if (status === google.maps.places.PlacesServiceStatus.OK) {
+			for (var i = 0; i < results.length; i++) {
+				createMarker(results[i]);
+			}
+		}
+	}
+
+	function createMarker(place) {
+		var placeLoc = place.geometry.location;
+		var marker = new google.maps.Marker({
+			map: map,
+			position: place.geometry.location,
+			animation: google.maps.Animation.DROP
+		});
+
+		google.maps.event.addListener(marker, 'click', function () {
+			infowindow.setContent(place.name);
+			infowindow.open(map, this);
+		});
+	}
+	
+	
+});
