d11150e7372674f238b3ab7838e0c2a37faa6704
Amardeep K <hammerstrike@users.noreply.github.com>
2016-06-22 20:12:39 +0530

    template added

---
+++ whatsnear/js/slpanels.js
@@ -0,0 +1,9 @@
+(function(window,$){
+    
+    //hamburger
+    $('#hamburger')
+        .on('click',function(){
+            $('html').toggleClass('sl-opening');
+        })
+    
+}(window,jQuery));
